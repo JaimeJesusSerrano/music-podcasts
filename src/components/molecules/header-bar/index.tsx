1fc2da4cd46fcf47d3d9146521fd0a44e1a6fc7d
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2023-09-29 09:46:45 +0200

    feat: add LoadingSpinner

--- src/components/molecules/header-bar/index.tsx
+++ src/components/molecules/header-bar/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useTranslation } from 'react-i18next'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 import routes from 'config/routes'
+import LoadingSpinner from 'components/atoms/loading-spinner'
 
 const HeaderBar = (): JSX.Element => {
+  const location = useLocation()
   const { t } = useTranslation()
 
+  const [isLocationChanged, setIsLocationChanged] = useState(true)
+
+  useEffect(() => {
+    setIsLocationChanged(true)
+    const timer = setTimeout(() => setIsLocationChanged(false), 1000)
+    return () => clearTimeout(timer)
+  }, [location.pathname])
+
   return (
-    <div className='h-10 flex items-center pl-3 border-b-2 text-xl text-cyan-500 font-medium'>
+    <div className='h-10 flex justify-between items-center px-3 border-b-2 text-xl text-cyan-500 font-medium'>
       <Link to={routes.home.path}>{t('component.header.title')}</Link>
+      <div>{isLocationChanged ? <LoadingSpinner /> : ''}</div>
     </div>
   )
 }
