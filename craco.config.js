7e3a7621268702444438b846540a5a8cdcb8581a
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2023-10-01 14:16:01 +0200

    chore: on build:production set maxChunks = 1

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require('webpack')
+
 module.exports = {
   style: {
     postcss: {
       mode: 'file',
+    },
+  },
+  webpack: {
+    plugins: {
+      add: [
+        new webpack.optimize.LimitChunkCountPlugin({
+          maxChunks: 1,
+        }),
+      ],
     },
   },
 }
